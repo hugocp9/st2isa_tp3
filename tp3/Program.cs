37c2408629dbc278f43699d4668956766263fd77
hugocp9 <hv8laptop@gmail.com>
2021-10-02 10:18:38 +0200

    init

---
+++ tp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
